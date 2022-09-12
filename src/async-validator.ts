e3ea2c4728bdb9aff370d5bfeae2449a904a4bb7
zhouhuafei <1123486116@qq.com>
2022-09-12 23:41:58 +0800

    feat: 🎸 单独校验某一条数据 - 配置keys存在限制 - 所以只能单独再进行一次校验

--- src/async-validator.ts
+++ src/async-validator.ts
@@ -1,9 +1,16 @@
-import Schema, { ValidateOption, Rules } from 'async-validator'
+import Schema, { ValidateOption, Rules, RuleItem } from 'async-validator'
 
-async function validateAllFormFields (formFields, formRules, option: ValidateOption = {}) {
+export { Schema, ValidateOption, Rules, RuleItem }
+
+// 校验表单中的所有字段
+export async function validateAllFormFields (formFields, formRules, option: ValidateOption = {}) {
+  // option.keys = ['name'] // 只校验name，适用于只校验某一项字段的场景，但是，但是，但是，对嵌套的属性无效。所以有点鸡肋。
   option.first = option.first || false // 开启后，若某个字段校验没通过，则停止下一个字段的校验。
-  option.firstFields = option.firstFields || false // 开启后，若某个字段的某个规则校验没通过，则停止下一个规则的校验，而后继续下一个字段的校验。
-  return await new Schema(formRules).validate(formFields, option).then(() => undefined).catch((err) => err.errors)
+  option.firstFields = option.firstFields || true // 开启后，若某个字段的某个规则校验没通过，则停止下一个规则的校验，而后继续下一个字段的校验。
+  return await new Schema(formRules)
+    .validate(formFields, option)
+    .then(() => undefined)
+    .catch((err) => err.errors.reduce((c, v) => {
+      return { ...c, [v.field]: v.message }
+    }, {}))
 }
-
-export { validateAllFormFields, Rules }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { validateAllFormFields, Rules } from './async-validator'
+import { Rules, RuleItem, validateAllFormFields } from './async-validator'
 
 const formFields = {
   id: undefined,
@@ -7,66 +7,65 @@ const formFields = {
   ext: { source: '' },
   specifications: []
 }
+const getInventoryRule = (): RuleItem[] => {
+  const type = 'number'
+  const min = 2
+  const max = 6
+  return [
+    { type: type, required: true, message: '库存必填' },
+    { type, min, max, message: `至少${min}件库存 至多${max}件库存` }
+  ]
+}
 const formRules: Rules = {
-  name: [
-    {
-      required: true,
-      // 先去除首尾空格再进行校验 - 不会改变原始值 - 打接口时还需自行处理首尾空格或在输入框输入时不允许输入首尾空格
-      whitespace: true,
-      message: '名称必填'
-    }
-  ],
-  weight: [
-    {
-      // 非嵌套的时候 - type是number - 不建议填写 - 否则填入任何字符串都报必填
-      required: true,
-      message: '重量必填'
-    },
-    {
-      validator: (rule, value, cb) => {
-        value = +value
-        const max = 9
-        const min = 1
-        if (isNaN(value)) {
-          return cb('重量需要输入数字')
-        } else if (value > max) {
-          return cb(`重量不能大于${max}`)
-        } else if (value < min) {
-          return cb(`重量不能小于${min}`)
-        }
-        cb()
+  name: (() => {
+    const type = 'string'
+    return [
+      {
+        // 不加type属性时，type默认是'string'。填写个0，false啥的不会报必填。会把其他类型转成字符串。
+        // 不加type属性时，type默认是'string'。但是若存在whitespace属性，不管其值是false还是true。填写个0，false啥的会报必填。不会把其他类型转成字符串。
+        // 加了type属性时，当其值为undefined或者'string'时。填写个0，false啥的会报必填。不会把其他类型转成字符串。
+        type,
+        required: true,
+        // 先去除首尾空格再进行校验，不会改变原始值，打接口时还需自行处理首尾空格或在输入框输入时不允许输入首尾空格。
+        whitespace: true,
+        message: '名称必填'
       }
-    }
-  ],
-  ext: { type: 'object', fields: { source: [{ required: true, message: '来源必填' }] } },
+    ]
+  })(),
+  weight: (() => {
+    const type = 'number'
+    return [
+      { type, required: true, message: '重量必填' },
+      {
+        type,
+        validator: (rule, value, cb) => {
+          value = +value
+          const max = 9
+          const min = 1
+          if (isNaN(value)) {
+            return cb('重量需要输入数字')
+          } else if (value > max) {
+            return cb(`重量不能大于${max}`)
+          } else if (value < min) {
+            return cb(`重量不能小于${min}`)
+          }
+          cb()
+        }
+      }
+    ]
+  })(),
+  ext: (() => {
+    const type = 'string'
+    return { type: 'object', fields: { source: [{ type, required: true, message: '来源必填' }] } }
+  })(),
   specifications: (() => {
+    const type = 'array' // 不配置这个则min和max校验通不过。还有个len。他们三个针对数组和字符串时，表示长度。针对数字时表示大小。
     const min = 3
     const max = 6
-    const type = 'array' // 不配置这个则min和max校验通不过
-    const inventoryMin = 2
-    const inventoryMax = 6
-    const inventoryType = 'number'
     return [
       { type, required: true, message: '规格必填' },
       { type, min, max, message: `至少${min}个规格 至多${max}个规格` },
-      {
-        type,
-        defaultField: {
-          type: 'object',
-          fields: {
-            inventory: [
-              // 嵌套的时候 - type是number - 需要填写 - 否则填入任何数字都报必填
-              { type: inventoryType, required: true, message: '库存必填' },
-              {
-                type: inventoryType,
-                min: inventoryMin,
-                max: inventoryMax,
-                message: `至少${inventoryMin}件库存 至多${inventoryMax}件库存`
-              }
-            ]
-          }
-        }
-      }
+      { type, defaultField: { type: 'object', fields: { inventory: getInventoryRule() } } }
     ]
   })()
 }
@@ -101,6 +100,26 @@ async function save () {
     console.log('校验未通过', formErrors)
   } else {
     console.log('校验通过')
+  }
+  console.log('\n======================== 分割线 ========================\n')
+  console.log('模拟再次进入编辑页，接口响应了之后，编辑了第一条规格的库存后，对第一条规格的库存进行校验')
+  formFields.specifications[0].inventory = 4
+  const tempName = 'specifications.0.inventory'
+  const tempValue = formFields.specifications[0].inventory
+  const tempFormFields = { [tempName]: tempValue }
+  const tempFormRules = { [tempName]: getInventoryRule() }
+  const tempFormErrors = await validateAllFormFields(tempFormFields, tempFormRules)
+  console.log('单独被校验的数据', tempFormFields)
+  if (tempFormErrors) {
+    console.log('单独校验未通过', formErrors)
+  } else {
+    console.log('单独校验通过')
+    delete formErrors[tempName]
+  }
+  if (formErrors) {
+    console.log('整体校验未通过', formErrors)
+  } else {
+    console.log('整体校验通过')
   }
 }
 
