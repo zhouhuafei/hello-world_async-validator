5cd1265a503d7ab6620cc33eeaec62eb3f031ac7
zhouhuafei <1123486116@qq.com>
2022-09-12 11:35:12 +0800

    feat: 🎸 校验对象嵌套对象 - 校验数组嵌套对象

---
+++ src/async-validator.ts
@@ -0,0 +1,9 @@
+import Schema, { ValidateOption, Rules } from 'async-validator'
+
+async function validationTrigger (formFields, formRules, option: ValidateOption = {}) {
+  option.first = option.first || false // 开启后，若某个字段校验没通过，则停止下一个字段的校验。
+  option.firstFields = option.firstFields || false // 开启后，若某个字段的某个规则校验没通过，则停止下一个规则的校验，而后继续下一个字段的校验。
+  return await new Schema(formRules).validate(formFields, option).then(() => undefined).catch((err) => err.errors)
+}
+
+export { validationTrigger, Rules }
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,24 @@
-import Schema, { Rules } from 'async-validator'
-
-async function validationTrigger (formFields, formRules) {
-  return await new Schema(formRules).validate(formFields, {
-    first: false, // 开启后，若某个字段校验没通过，则停止下一个字段的校验。
-    firstFields: false // 开启后，若某个字段的某个规则校验没通过，则停止下一个规则的校验，而后继续下一个字段的校验。
-  }).then(() => undefined).catch((err) => err.errors)
-}
+import { validationTrigger, Rules } from './async-validator'
 
 const formFields = {
   id: undefined,
   name: '',
   weight: '',
+  ext: { source: '' },
   specifications: []
 }
 const formRules: Rules = {
   name: [
     {
       required: true,
-      whitespace: true, // 先去除首尾空格再进行校验 - 不会改变原始值 - 打接口时还需自行处理首尾空格或在输入框输入时不允许输入首尾空格
+      // 先去除首尾空格再进行校验 - 不会改变原始值 - 打接口时还需自行处理首尾空格或在输入框输入时不允许输入首尾空格
+      whitespace: true,
       message: '名称必填'
     }
   ],
   weight: [
     {
-      // 虽然期望值是number - 但是并不建议配置上type - 否则填入任何字符串都报必填
+      // 非嵌套的时候 - type是number - 不建议填写 - 否则填入任何字符串都报必填
       required: true,
       message: '重量必填'
     },
@@ -43,10 +38,14 @@ const formRules: Rules = {
       }
     }
   ],
+  ext: { type: 'object', fields: { source: [{ required: true, message: '来源必填' }] } },
   specifications: (() => {
     const min = 3
     const max = 6
     const type = 'array' // 不配置这个则min和max校验通不过
+    const inventoryMin = 2
+    const inventoryMax = 6
+    const inventoryType = 'number'
     return [
       { type, required: true, message: '规格必填' },
       { type, min, max, message: `至少${min}个规格 至多${max}个规格` },
@@ -56,7 +55,14 @@ const formRules: Rules = {
           type: 'object',
           fields: {
             inventory: [
-              { type: 'number', required: true, message: '库存必填' }
+              // 嵌套的时候 - type是number - 需要填写 - 否则填入任何数字都报必填
+              { type: inventoryType, required: true, message: '库存必填' },
+              {
+                type: inventoryType,
+                min: inventoryMin,
+                max: inventoryMax,
+                message: `至少${inventoryMin}件库存 至多${inventoryMax}件库存`
+              }
             ]
           }
         }
@@ -81,10 +87,11 @@ async function save () {
     id: 1,
     name: '商品名称',
     weight: 5,
+    ext: { source: '京东' },
     specifications: [
-      { id: 11, name: '红色', inventory: 2 },
-      { id: 12, name: '蓝色', inventory: 2 },
-      { id: 13, name: '紫色', inventory: 2 }
+      { id: 11, name: '红色', inventory: 9 },
+      { id: 12, name: '蓝色', inventory: 9 },
+      { id: 13, name: '紫色', inventory: 9 }
     ]
   })
   const editRes = await validationTrigger(formFields, formRules)
@@ -97,6 +104,3 @@ async function save () {
 }
 
 save().then()
-
-// ...TODO 如何只校验某一个字段？ - 摘取规则中的某一项？
-// ...TODO 如何只校验数组中对象的某一个字段？ - 怎么摘取？
